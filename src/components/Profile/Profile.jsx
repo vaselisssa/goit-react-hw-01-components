7b0c16e5749a49aac84de5245239514bfd1517a9
Svitlana Vasylkovska <vaselisssa@gmail.com>
2023-09-18 03:21:04 +0300

    add Profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,56 @@
+import { AiFillHeart, AiOutlineEye } from 'react-icons/ai';
+import { BsPeopleFill } from 'react-icons/bs';
+
+import {
+  UserProfile,
+  Description,
+  Avatar,
+  Name,
+  UserInfo,
+  StatsList,
+  StatsItem,
+  StatsLabel,
+  StatsQuantity,
+} from './Profile.styled';
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <UserProfile>
+      <Description>
+        <Avatar src={avatar} alt={username} />
+        <Name>{username}</Name>
+        <UserInfo>@{tag}</UserInfo>
+        <UserInfo>{location}</UserInfo>
+      </Description>
+      <StatsList>
+        <StatsItem>
+          <StatsLabel>
+            <BsPeopleFill />
+            Followers
+          </StatsLabel>
+          <StatsQuantity>{followers}</StatsQuantity>
+        </StatsItem>
+        <StatsItem>
+          <StatsLabel>
+            <AiOutlineEye />
+            Views
+          </StatsLabel>
+          <StatsQuantity>{views}</StatsQuantity>
+        </StatsItem>
+        <StatsItem>
+          <StatsLabel>
+            <AiFillHeart />
+            Likes
+          </StatsLabel>
+          <StatsQuantity>{likes}</StatsQuantity>
+        </StatsItem>
+      </StatsList>
+    </UserProfile>
+  );
+};
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,75 @@
+import styled from '@emotion/styled';
+
+export const UserProfile = styled.div`
+  margin: 60px auto;
+  width: 360px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f2eeed;
+  box-shadow: 0px 5px 14px -2px rgba(0, 0, 0, 0.99);
+`;
+
+export const Description = styled.div`
+  padding: 40px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Avatar = styled.img`
+  width: 75%;
+  height: 75%;
+  border-radius: 50%;
+  border: 1px solid #6e6a69;
+`;
+
+export const Name = styled.p`
+  margin-top: 30px;
+  font-size: 36px;
+  font-weight: 700;
+`;
+
+export const UserInfo = styled.p`
+  margin-top: 10px;
+  font-size: 20px;
+  color: #5e5152;
+`;
+
+export const StatsList = styled.ul`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #8a8584;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  background-color: #deced0;
+`;
+
+export const StatsItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+
+  &:not(:last-child) {
+    border-right: 1px solid #8a8584;
+  }
+`;
+
+export const StatsLabel = styled.span`
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  color: #5e5152;
+  font-size: 16px;
+`;
+
+export const StatsQuantity = styled.span`
+  font-size: 24px;
+  font-weight: 700;
+`;
